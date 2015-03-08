8a4d56b57e4c0ab9d1be0bae5c8a890e57385110
Peter Cai <xqsx43cxy@126.com>
2015-03-08 16:50:38 +0800

    Xposed module init

---
+++ app/src/main/java/info/papdt/swipeback/mod/ModSwipeBack.java
@@ -0,0 +1,14 @@
+package info.papdt.swipeback.mod;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class ModSwipeBack implements IXposedHookLoadPackage
+{
+
+	@Override
+	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+		
+	}
+
+}
