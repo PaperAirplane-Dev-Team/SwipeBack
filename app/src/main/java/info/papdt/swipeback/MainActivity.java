6a55b40dd94dc95e63fab35f723f2c1a243893ae
Peter Cai <xqsx43cxy@126.com>
2015-03-08 15:54:41 +0800

    Initial commit

---
+++ app/src/main/java/info/papdt/swipeback/MainActivity.java
@@ -0,0 +1,14 @@
+package info.papdt.swipeback;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
