7a1f8eeffbc76c530b3ddd4e6962e8d0a2b9419f
Peter Cai <xqsx43cxy@126.com>
2015-03-09 22:41:11 +0800

    SwipeBackLayout: set decor background to 0 but not null

--- libraries/SwipeBackLayout/src/main/java/me/imid/swipebacklayout/lib/app/SwipeBackActivityHelper.java
+++ libraries/SwipeBackLayout/src/main/java/me/imid/swipebacklayout/lib/app/SwipeBackActivityHelper.java
@@ -45,7 +45,7 @@ public class SwipeBackActivityHelper {
     public void onPostCreate() {
         mSwipeBackLayout.attachToActivity(mActivity);
         mActivity.getWindow().setBackgroundDrawable(new ColorDrawable(0));
-		mActivity.getWindow().getDecorView().setBackground(null);
+		mActivity.getWindow().getDecorView().setBackgroundColor(0);
     }
 
     public View findViewById(int id) {
