6a0660920eacad94fbbc976ffea2d95dec951215
Peter Cai <xqsx43cxy@126.com>
2015-04-02 22:49:16 +0800

    PerActivityFragment: Enable auto-selecting default class

--- app/src/main/java/info/papdt/swipeback/ui/app/PerActivityFragment.java
+++ app/src/main/java/info/papdt/swipeback/ui/app/PerActivityFragment.java
@@ -17,7 +17,8 @@ import info.papdt.swipeback.ui.model.ActivityModel;
 public class PerActivityFragment extends BaseListFragment<ActivityModel>
 {
 	private BaseAdapter mAdapter;
-	private String mTitle = "";
+	private String mTitle = "", mDefaultClassName, mPkgName;
+	private int mDefaultClassPosition = -1;
 
 	@Override
 	protected BaseAdapter buildAdapter() {
@@ -30,10 +31,18 @@ public class PerActivityFragment extends BaseListFragment<ActivityModel>
 		List<ActivityModel> list = new ArrayList<ActivityModel>();
 		PackageManager pm = getActivity().getPackageManager();
 		
+		if (getExtraPass().contains(",")) {
+			String[] s = getExtraPass().split(",");
+			mPkgName = s[0];
+			mDefaultClassName = s[1];
+		} else {
+			mPkgName = getExtraPass();
+		}
+		
 		ActivityInfo[] ai;
 		
 		try {
-			PackageInfo pkg = pm.getPackageInfo(getExtraPass(), PackageManager.GET_ACTIVITIES);
+			PackageInfo pkg = pm.getPackageInfo(mPkgName, PackageManager.GET_ACTIVITIES);
 			ai = pkg.activities;
 			mTitle = pm.getApplicationLabel(pkg.applicationInfo).toString();
 		} catch (Exception e) {
@@ -54,6 +63,10 @@ public class PerActivityFragment extends BaseListFragment<ActivityModel>
 				activity.title = info.loadLabel(pm).toString();
 				list.add(activity);
 				callback.updateProgress(list.size() - 1, ai.length);
+				
+				if (activity.className.equals(mDefaultClassName)) {
+					mDefaultClassPosition = list.size() - 1;
+				}
 			}
 		}
 		
@@ -68,9 +81,12 @@ public class PerActivityFragment extends BaseListFragment<ActivityModel>
 			setTitle(mTitle + " - " + getString(R.string.app_name));
 		}
 		
+		if (mDefaultClassPosition > 0) {
+			onItemClick(mDefaultClassPosition);
+		}
 		// If size is smaller than 2, which means the app has only one or no activity
 		// Then we can skip this fragment and go to settings directly
-		if (data.size() <= 2) {
+		else if (data.size() <= 2) {
 			onItemClick(0);
 			getActivity().finish();
 		}
@@ -79,7 +95,7 @@ public class PerActivityFragment extends BaseListFragment<ActivityModel>
 	@Override
 	protected void onItemClick(int pos) {
 		ActivityModel activity = getItemList().get(pos);
-		startFragment("settings", getExtraPass() + "," + activity.className + "," + activity.title + "," + mTitle);
+		startFragment("settings", mPkgName + "," + activity.className + "," + activity.title + "," + mTitle);
 	}
 
 }
